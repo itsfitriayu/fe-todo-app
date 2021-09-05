62362b6328b96c2e497f8017867a0f90ba3ad7b5
Fitri Ayu <fitriayu.cahyaningrum@gmail.com>
2021-09-05 22:50:43 +0700

    [FIT] add login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import Form from "./Components/Form";
-import "./App.css";
 
-const App = () => {
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import "./App.css";
+import Form from "./Components/Form"; //registration form (signup)
+import Login from "./Components/login"; //login page (signin)
+
+function App () {
   return (
+    /* <router>
+        <switch>
+          <Route exact path="/" component={login} />
+          <Route exact path="/signup" component={Form} />
+        </switch>
+      </router> */
+      
     <div>
-      <Form />
-      </div>
+    <Login/>
+    </div>
   );
 };
 
