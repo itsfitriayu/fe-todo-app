ceec3e7e10c59e3a76e0fc477661c08b61d1bf89
amelizia <87222341+amelizia@users.noreply.github.com>
2021-09-08 04:59:12 +0700

    Fixed merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
 
 import "./App.css";
 import Form from "./Components/Form"; //registration form (signup)
 import Login from "./Components/login"; //login page (signin)
 import NotFound from "./Components/NotFound"; //404 not found
 import Forgot from "./Components/forgot"; //forgot-passw
+import ListItems from './ListItems'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-
-
+library.add(faTrash)
 function App () {
   return (
     <Router>
@@ -20,6 +22,84 @@ function App () {
     </Switch>
   </Router>
   );
-};
+}
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.addItem = this.addItem.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem.text !==""){
+      const items = [...this.state.items, newItem];
+    this.setState({
+      items: items,
+      currentItem:{
+        text:'',
+        key:''
+      }
+    })
+    }
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  deleteItem(key){
+    const filteredItems= this.state.items.filter(item =>
+      item.key!==key);
+    this.setState({
+      items: filteredItems
+    })
+
+  }
+  setUpdate(text,key){
+    console.log("items:"+this.state.items);
+    const items = this.state.items;
+    items.map(item=>{      
+      if(item.key===key){
+        console.log(item.key +"    "+key)
+        item.text= text;
+      }
+    })
+    this.setState({
+      items: items
+    })
+    
+   
+  }
+ render(){
+  return (
+    <div className="App">
+      <header>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
+          <button type="submit">Add</button>
+        </form>
+        <p>{this.state.items.text}</p>
+        
+          <ListItems items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate}/>
+        
+      </header>
+    </div>
+  );
+ }
+}
+
 
 export default App;
