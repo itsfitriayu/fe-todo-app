d794e6c0260f610c7ce86b468996fa890eea5328
Fitri Ayu <fitriayu.cahyaningrum@gmail.com>
2021-09-09 20:02:32 +0700

    [FIT] login and regist

--- src/Components/login.js
+++ src/Components/login.js
@@ -2,12 +2,46 @@ import React, {useState}  from "react";
 import { Link } from 'react-router-dom';
 
 const Loginform = () => {
-  const [details, setDetails] = useState({email:"", password:""});
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
+  const [rememberToken, setRememberToken] = useState("");
+  const [error, setError] = useState(null);
 
-  const submitHandler = e => {
-    e.preventDefault();  
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  }
+    const signIn = {
+      email,
+      password,
+    };
+
+ fetch("http://bc21-180-252-118-200.ngrok.io/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(signIn),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if (data.status === "error") {
+          throw Error(data.message);
+        } else {
+          setError(null);
+          setToken(data.token);
+          setToken(data.rememberToken);
+          if (typeof Storage !== "undefined") {
+            localStorage.setItem("logged", "true");
+          }
+         
+        }
+        console.log(data);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-white py-12 px-4 sm:px-6 lg:px-8">
@@ -25,9 +59,10 @@ const Loginform = () => {
           <div  className="p-5 rounded-md bg-gray-100">
            <h2 className="mt-6 text-xl text-gray-900">Great to see you again!</h2>
             
-          <form onSubmit={submitHandler} method="post"
+          <form onSubmit={handleSubmit} 
           className="mt-8 space-y-6">
             <input type="hidden" name="remember" defaultValue="true" />
+            {error !== null && <p className="mt-1 text-right text-indigo-500">{error}</p>}
             <div>
               <div className="rounded-md -space-y-px">
                 <label htmlFor="email" className="sr-only">
@@ -41,8 +76,8 @@ const Loginform = () => {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 placeholder-gray-400 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Email"
-                  onChange={e => setDetails({...details, email: e.target.value})}
-                  value={details.email}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div className="mt-3">
@@ -58,12 +93,13 @@ const Loginform = () => {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 placeholder-gray-400 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
-                  onChange={e => setDetails({...details, password: e.target.value})}
-                  value={details.password}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
             </div>
             <div>
+            
               <button
                 type="submit"
                 className="mt-12 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-800 hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
